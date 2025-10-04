b0770ec2875030286cde8691873d71fb090f263e
Dave Medrano <evadonardem@gmail.com>
2025-10-04 10:04:32 +0800

    Updated template rolling and rendering the dice.

--- snake_and_ladders/scripts/snake_and_ladders.js
+++ snake_and_ladders/scripts/snake_and_ladders.js
@@ -22,3 +22,20 @@ class Die {
      * const die = new Die(6);
      */
 }
+
+
+const diceElement = document.getElementById('dicePlaceholder');
+const rollDiceButton = document.getElementById('rollDiceButton');
+const dice = new Die(6);
+
+rollDiceButton.addEventListener('click', () => {
+    /**
+     * Complete the event listener to meet the following requirements:
+     * render the icon of the die in the diceElement when the button is clicked.
+     * Use the getIcon method of the Die class to get the appropriate icon based on the number of sides.
+     * For example, if the die has 6 sides, it should render '<i class="fa fa-dice-six"></i>'.
+     */
+    const icon = 'dice-five';
+    diceElement.innerHTML = `<i class="fa fa-2xl fa-${icon}"></i>`;
+});
+

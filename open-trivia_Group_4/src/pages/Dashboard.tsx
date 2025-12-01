a2490d63d66b7283c462fcf31017b6538949136f
Daryll Daniel <d412yll@gmail.com>
2025-12-01 16:35:26 +0800

    02_React_OpenTrivia_Group_4

---
+++ open-trivia_Group_4/src/pages/Dashboard.tsx
@@ -0,0 +1,8 @@
+const Dashboard = () => {
+    return <>
+        <h1>Dashboard</h1>
+        <p>Under construction...</p>
+    </>;
+};
+
+export default Dashboard;
---
+++ open-trivia_Group_4/src/pages/Favorites.tsx
@@ -0,0 +1,8 @@
+const Favorites = () => {
+    return <>
+        <h1>Favorites</h1>
+        <p>Under construction...</p>
+    </>;
+};
+
+export default Favorites;
